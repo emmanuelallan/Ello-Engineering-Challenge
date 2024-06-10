8cf3283c5f67689ea665ad98662db79dad76ae27
emmanuelallan <imma.allan@gmail.com>
2024-06-10 18:16:17 +0300

    add notification

---
+++ frontend/src/components/Toast.tsx
@@ -0,0 +1,22 @@
+import { Snackbar } from "@mui/joy";
+import { NotificationMessage } from '../types.ts'
+
+interface ToastProps {
+    notification: NotificationMessage | null;
+    onClose?: () => void;
+}
+
+export default function Toast({ notification, onClose }: ToastProps) {
+    return (
+        <Snackbar
+            autoHideDuration={3000}
+            color={notification?.type || "neutral"}
+            size="lg"
+            variant="outlined"
+            open={!!notification}
+            onClose={onClose}
+        >
+            {notification?.message}
+        </Snackbar>
+    );
+}
